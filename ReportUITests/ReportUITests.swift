8b8e63481ee38d5ca7551b7afee10f87960ea00b
Hoang <hoangtoro102@gmail.com>
2020-07-03 18:04:53 +0700

    Add simple UI test.

--- ReportUITests/ReportUITests.swift
+++ ReportUITests/ReportUITests.swift
@@ -10,26 +10,17 @@ import XCTest
 
 class ReportUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testTableInteraction() throws {
         let app = XCUIApplication()
         app.launch()
+        
+        XCTAssertTrue(app.waitForExistence(timeout: 1))
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let actExists = app.activityIndicators.firstMatch.waitForExistence(timeout: 30)
+        XCTAssertTrue(actExists)
+        
+        let tableView = app.tables.element.firstMatch
+        XCTAssertTrue(tableView.exists)
     }
 
     func testLaunchPerformance() throws {
